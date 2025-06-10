e9fb85b24fcb40634ba357ca6bd85f709f66294e
Basil Aleksandrov <basilaleks@MacBook-Pro-Basil-409.local>
2025-06-10 17:14:46 +0300

    add offer method handlers

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -7,3 +7,4 @@ export * from './mock-server-data.type.js';
 export * from './component.enum.js';
 export * from './category.type.js';
 export * from './user.type.js';
+export * from './sort-type.enum.js';
